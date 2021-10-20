d86a21c20df6500aec3c14de54c8a55e5e5dd5a7
eparzych <emilia.parzych92@gmail.com>
2021-10-20 12:59:04 +0200

    Added login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -21,10 +21,27 @@ export const Login = (props) => {
     }
     
     return (
-    <form>
-      <input type="text" name="userName" value={form.userName} onChange={handleChange}/>
-      <input type="password" name="password" value={form.password} onChange={handleChange}/>
-      <button onClick={onSubmit}>Zaloguj</button>
-    </form>
+        <div className="login">
+            <div className="logo">
+                <div class="logo__icon">
+                    <i></i>
+                    <span></span>
+                </div>
+            </div>
+            <form className="form">
+                <h1 className="form__title">Welcome</h1>
+                <p className="form__subtitle">to our website</p>
+                <p className="form__text">Log in</p>
+                <label for="">
+                    User name
+                    <input className="user__name" type="text" name="userName" value={form.userName} onChange={handleChange}/>
+                </label>
+                <label for="">
+                    Password
+                    <input className="password" type="password" name="password" value={form.password} onChange={handleChange}/>
+                </label>
+                <div className="btn" onClick={onSubmit}><button className="btn__login">Zaloguj</button></div>
+            </form>
+        </div>
   );
 }
